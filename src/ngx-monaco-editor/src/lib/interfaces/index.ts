d696b5396dc8008f25d4c1aa88cd013f837ebf65
Michael Rentmeister <MRentmeister@experlogix.com>
2021-11-15 10:08:04 -0700

    fixed monaco path

--- src/ngx-monaco-editor/src/lib/interfaces/index.ts
+++ src/ngx-monaco-editor/src/lib/interfaces/index.ts
@@ -1,4 +1,4 @@
-/// <reference path="monaco.d.ts" />
+/// <reference path="../../../../../node_modules/monaco-editor/monaco.d.ts" />
 declare const monaco: any;
 export const MONACO_PATH = 'MONACO_PATH';
 export type MonacoEditorConstructionOptions = monaco.editor.IStandaloneEditorConstructionOptions;
